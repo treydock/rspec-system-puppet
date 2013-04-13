0c5b24c31d3abec1040edd6dd5bcf467f25c84d8
Ken Barber <ken@bob.sh>
2013-04-13 02:05:42 +0100

    Add puppet_apply helper
    Signed-off-by: Ken Barber <ken@bob.sh>

--- spec/system/basic_spec.rb
+++ spec/system/basic_spec.rb
@@ -5,10 +5,34 @@ describe "basic tests:" do
     puppet_install()
   end
 
+  it 'facter domain should return something valid' do
+    system_run("facter domain") do |r|
+      r[:stdout].should =~ /[a-z]+/
+      r[:stderr].should == ''
+      r[:exit_code].should == 0
+    end
+  end
+
+  it 'facter fqdn should return something valid' do
+    system_run("facter fqdn") do |r|
+      r[:stdout].should =~ /vm/
+      r[:stderr].should == ''
+      r[:exit_code].should == 0
+    end
+  end
+
   it 'check puppet_resource returns an exit code of 0' do
     puppet_resource('user') do |r|
-      r[:exit_code].should == 0
       r[:stderr].should == ''
+      r[:exit_code].should == 0
+    end
+  end
+
+  it 'check puppet apply with just a notice' do
+    puppet_apply('notice("foo")') do |r|
+      r[:stdout].should =~ /foo/
+      r[:stderr].should == ''
+      r[:exit_code].should == 0
     end
   end
 end
