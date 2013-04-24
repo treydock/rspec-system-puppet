1d76b7f1a15478ea24b96bebdc39c67368bd0060
Ken Barber <ken@bob.sh>
2013-04-24 21:25:10 +0100

    Make sure we lay down a base hiera.yaml
    Signed-off-by: Ken Barber <ken@bob.sh>

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -1,6 +1,10 @@
 require 'rspec-system/spec_helper'
 require 'rspec-system-puppet/helpers'
 
+def proj_root
+  Pathname.new(File.join(File.dirname(__FILE__), '..'))
+end
+
 RSpec.configure do |c|
   c.include RSpecSystemPuppet::Helpers
 end
